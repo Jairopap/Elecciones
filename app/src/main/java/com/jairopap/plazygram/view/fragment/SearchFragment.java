1e56721471e7a1c68deed1b113869b4eec6e63d6
Jairo Pacanchique <jairopap@hotmail.com>
2017-08-03 13:55:03 -0500

    Cree en resultados el recliclre vie fata poner ya solamente crear el adapater y cambiar pictere a resultados

--- app/src/main/java/com/jairopap/plazygram/view/fragment/SearchFragment.java
+++ app/src/main/java/com/jairopap/plazygram/view/fragment/SearchFragment.java
@@ -1,14 +1,33 @@
 package com.jairopap.plazygram.view.fragment;
 
 
+import android.app.DownloadManager;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.jairopap.plazygram.R;
+import com.jairopap.plazygram.adapter.PictureAdapterRecyclerView;
+import com.jairopap.plazygram.model.Picture;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 /**
  * A simple {@link Fragment} subclass.
  */
@@ -24,7 +43,36 @@ public class SearchFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        View view = inflater.inflate(R.layout.fragment_search, container, false);
+        showToolbar(getResources().getString(R.string.tab_search), false, view);
+
+        RecyclerView picturesRecycler = (RecyclerView) view.findViewById(R.id.pictureRecycler);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+
+
+        picturesRecycler.setLayoutManager(linearLayoutManager);
+
+        PictureAdapterRecyclerView pictureAdapterRecyclerView =
+                new PictureAdapterRecyclerView(buildPictures(), R.layout.cardview_picture, getActivity());
+        picturesRecycler.setAdapter(pictureAdapterRecyclerView);
+        return view;
+    }
+    public ArrayList<Picture> buildPictures(){
+
+        ArrayList<Picture> pictures = new ArrayList<>();
+        pictures.add(new Picture("@drawable/image", "Uriel Ramírez", "4 días", "3 Me Gusta"));
+
+        pictures.add(new Picture("http://www.enjoyart.com/library/landscapes/tuscanlandscapes/large/Tuscan-Bridge--by-Art-Fronckowiak-.jpg", "Juan Pablo", "3 días", "10 Me Gusta"));
+        pictures.add(new Picture("http://www.educationquizzes.com/library/KS3-Geography/river-1-1.jpg", "Anahi Salgado", "2 días", "9 Me Gusta"));
+        return pictures;
+    }
+
+    private void showToolbar(String tittle, boolean upButton, View view) {
+        Toolbar toolbar =(Toolbar) view.findViewById(R.id.toolbar);
+        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(tittle);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
     }
 
 }
